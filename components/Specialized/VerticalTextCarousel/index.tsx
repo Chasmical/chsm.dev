2cfb3b9fcf266e89ec81ede3cdc9dcdf7ba267df
Chasmical <hillankeyxl2@yandex.ru>
2025-01-17 22:37:46 +0700

    ✨ Add a VerticalTextCarousel component

---
+++ components/Specialized/VerticalTextCarousel/index.tsx
@@ -0,0 +1,101 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import useLatest from "@lib/hooks/useLatest";
+import useEvent from "@lib/hooks/useEvent";
+import styles from "./index.module.scss";
+import clsx from "clsx";
+
+const optionViewOffset = 3;
+
+interface VerticalTextCarouselProps {
+  options: React.ReactNode[];
+}
+export default function VerticalTextCarousel({ options }: VerticalTextCarouselProps) {
+  const lastScrollTime = useRef(0);
+  const optionsLength = useLatest(options.length);
+
+  const [scrollPos, setScrollPos] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const optionsBlockRef = useRef<HTMLDivElement>(null);
+
+  const smoothScrollListBy = (diff: number) => {
+    setScrollPos(oldPos => {
+      let next = oldPos + diff;
+      // If new position is within range, return it
+      if (next >= 0 && next < optionsLength.current) return next;
+
+      // Temporarily suppress the movement animation
+      const optionsBlock = optionsBlockRef.current!;
+      optionsBlock.style.transition = "none";
+
+      // Normalize the scroll position
+      if (next < 0) next += optionsLength.current;
+      if (next >= optionsLength.current) next -= optionsLength.current;
+
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          optionsBlock.style.removeProperty("transition");
+          setScrollPos(next);
+        });
+      });
+
+      return next + (diff > 0 ? -1 : 1);
+    });
+  };
+
+  useEffect(() => {
+    let timeoutRef: NodeJS.Timeout | number = 0;
+
+    const idleScroll = () => {
+      if (performance.now() - lastScrollTime.current < 3000) {
+        timeoutRef = setTimeout(idleScroll, 100);
+      } else {
+        smoothScrollListBy(1);
+        timeoutRef = setTimeout(idleScroll, 2500);
+      }
+    };
+
+    timeoutRef = setTimeout(idleScroll, 2000);
+    return () => clearTimeout(timeoutRef);
+  }, []);
+
+  useEvent(containerRef.current, "wheel", ev => {
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    const curTime = performance.now();
+    if (curTime - lastScrollTime.current < 600) return;
+    lastScrollTime.current = curTime;
+
+    smoothScrollListBy(ev.deltaY > 0 ? 1 : -1);
+  });
+
+  const items = options.map((opt, i) => {
+    return (
+      <div className={clsx(styles.option, i === scrollPos && styles.selected)} key={i}>
+        {opt}
+      </div>
+    );
+  });
+
+  return (
+    <div className={styles.wrapper}>
+      <div ref={containerRef} className={styles.container} style={{ "--scroll-offset": scrollPos }}>
+        <div ref={optionsBlockRef} className={styles.options}>
+          {items.slice(items.length - optionViewOffset)}
+          {items}
+          {items.slice(0, optionViewOffset + optionViewOffset)}
+        </div>
+        <div className={styles.mask} />
+        <div className={styles.shadowTop} />
+        <div className={styles.shadowBottom} />
+      </div>
+    </div>
+  );
+}
+
+declare module "csstype" {
+  interface Properties {
+    "--scroll-offset"?: number;
+  }
+}
