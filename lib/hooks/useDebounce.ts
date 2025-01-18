beadf2a676c746a8a416fc62c3214b501741d523
Chasmical <hillankeyxl2@yandex.ru>
2025-01-19 01:10:47 +0700

    ♻️ Overhaul useDebounce and useThrottle hooks

--- lib/hooks/useDebounce.ts
+++ lib/hooks/useDebounce.ts
@@ -1,39 +1,90 @@
 "use client";
-import useLatest from "@lib/hooks/useLatest";
-import { useCallback, useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 export default function useDebounce<Args extends unknown[]>(
-  callback: (...args: Args) => void,
+  callback: (...args: Args) => void | Promise<void>,
   waitMs: number,
-): [waiting: boolean, start: (...args: Args) => void, reset: () => void] {
-  const callbackRef = useLatest(callback);
-  const timeoutRef = useRef<NodeJS.Timeout | number>(0);
+): [state: DebounceState, run: (...args: Args) => void] {
+  const [state, setState] = useState<DebounceState>("idle");
 
-  const [waiting, setWaiting] = useState(false);
+  const debouncerRef = useRef<ReturnType<typeof createDebouncer<Args>>>(null);
+  const debounced = (debouncerRef.current ??= createDebouncer(setState));
 
-  const start = useCallback(
-    (...args: Args) => {
-      // If already debouncing, clear the timeout; otherwise, set waiting to true
-      timeoutRef.current ? clearTimeout(timeoutRef.current) : setWaiting(true);
+  debounced.__callback = callback;
+  debounced.__waitMs = waitMs;
 
-      // Re-set/start the timeout
-      timeoutRef.current = setTimeout(() => {
-        setWaiting(false);
-        timeoutRef.current = 0;
-        callbackRef.current(...args);
-      }, waitMs);
-    },
-    [waitMs],
-  );
+  return [state, debounced];
+}
 
-  const reset = useCallback(() => {
-    // If debouncing, reset everything
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-      timeoutRef.current = 0;
-      setWaiting(false);
+export function useDebounceStateless<Args extends unknown[]>(
+  callback: (...args: Args) => void | Promise<void>,
+  waitMs: number,
+): (...args: Args) => void {
+  const debouncerRef = useRef<ReturnType<typeof createDebouncer<Args>>>(null);
+  const debounced = (debouncerRef.current ??= createDebouncer());
+
+  debounced.__callback = callback;
+  debounced.__waitMs = waitMs;
+
+  return debounced;
+}
+
+export type DebounceState = "idle" | "queued" | "running";
+
+function createDebouncer<Args extends unknown[]>(setState?: (state: DebounceState) => void) {
+  let queuedArgs: Args | undefined;
+  let timeoutRef: NodeJS.Timeout | number = 0;
+  let lastCallTime = -9e99;
+
+  debounced.__callback = undefined! as (...args: Args) => void | Promise<void>;
+  debounced.__waitMs = undefined! as number;
+
+  return debounced;
+
+  function debounced(...args: Args) {
+    // set the queued args to the new args
+    queuedArgs = args;
+
+    // if currently running, do nothing else
+    if (!lastCallTime) return;
+
+    // clear the timeout, if waiting for the next call
+    if (timeoutRef) clearTimeout(timeoutRef);
+
+    // set "queued" state, and start the timeout for the queued call
+    setState?.("queued");
+    timeoutRef = setTimeout(popQueueAndRun, debounced.__waitMs);
+  }
+  function popQueueAndRun() {
+    // Clear the timeout id
+    timeoutRef = 0;
+    // Pop the next queued arguments
+    const nextArgs = queuedArgs!;
+    queuedArgs = undefined;
+    // Set the "running" state
+    setState?.("running");
+    // Set lastCallTime to 0, to indicate that the call is in progress
+    lastCallTime = 0;
+
+    try {
+      // Run the callback
+      const result = debounced.__callback(...nextArgs);
+      // When it's finished, set the last call time
+      result instanceof Promise ? result.then(setLastCallTime, setLastCallTime) : setLastCallTime();
+    } catch (err) {
+      setLastCallTime();
+      throw err;
     }
-  }, []);
-
-  return [waiting, start, reset];
+  }
+  function setLastCallTime() {
+    lastCallTime = performance.now();
+    // if there's already a next call queued, set "queued" state, and start the timeout again
+    if (queuedArgs) {
+      setState?.("queued");
+      timeoutRef = setTimeout(popQueueAndRun, debounced.__waitMs);
+    } else {
+      // nothing else queued, set "idle" state
+      setState?.("idle");
+    }
+  }
 }
--- lib/hooks/useThrottle.ts
+++ lib/hooks/useThrottle.ts
@@ -1,67 +1,100 @@
 "use client";
-import useLatest from "@lib/hooks/useLatest";
-import { useCallback, useRef, useState } from "react";
-
-export type ThrottleState = "idle" | "cooldown" | "queued";
+import { useRef, useState } from "react";
 
 export default function useThrottle<Args extends unknown[]>(
-  callback: (...args: Args) => void,
-  delayMs: number,
-): [state: ThrottleState, start: (...args: Args) => void, reset: (executeQueued?: boolean) => void] {
-  const callbackRef = useLatest(callback);
-  const timeoutRef = useRef<NodeJS.Timeout | number>(0);
-  const queuedArgsRef = useRef<Args>(null);
-
+  callback: (...args: Args) => void | Promise<void>,
+  waitMs: number,
+): [state: ThrottleState, run: (...args: Args) => void] {
   const [state, setState] = useState<ThrottleState>("idle");
 
-  // (no need to memo this func, since it only uses refs, and it's only referenced by `start`)
-  const waitForNextOne = () => {
-    // Start the "cooldown" state
-    setState("cooldown");
+  const throttlerRef = useRef<ReturnType<typeof createThrottler<Args>>>(null);
+  const throttled = (throttlerRef.current ??= createThrottler(setState));
 
-    timeoutRef.current = setTimeout(() => {
-      // If by the next time, there's another call queued, call it and wait again
-      const queuedArgs = queuedArgsRef.current;
-      if (queuedArgs) {
-        queuedArgsRef.current = null;
-        waitForNextOne();
-        callbackRef.current(...queuedArgs); // Call callback last, in case it throws
-      } else {
-        // Otherwise, go back to "idle" state
-        setState("idle");
-        timeoutRef.current = 0;
-      }
-    }, delayMs);
-  };
+  throttled.__callback = callback;
+  throttled.__waitMs = waitMs;
 
-  const start = useCallback(
-    (...args: Args) => {
-      if (timeoutRef.current) {
-        // If already throttling, just set or override the queued call's arguments
-        setState("queued");
-        queuedArgsRef.current = args;
-      } else {
-        // If not throttling, call the callback, and wait for the next one
-        waitForNextOne();
-        callbackRef.current(...args); // Call callback last, in case it throws
-      }
-    },
-    [delayMs],
-  );
+  return [state, throttled];
+}
 
-  const reset = useCallback((executeQueued = true) => {
-    // Go back to "idle" state, clear the timeout, stop the throttling
-    setState("idle");
-    clearTimeout(timeoutRef.current);
-    timeoutRef.current = 0;
+export function useThrottleStateless<Args extends unknown[]>(
+  callback: (...args: Args) => void | Promise<void>,
+  waitMs: number,
+): (...args: Args) => void {
+  const throttlerRef = useRef<ReturnType<typeof createThrottler<Args>>>(null);
+  const throttled = (throttlerRef.current ??= createThrottler());
 
-    // Clear the queued arguments, and execute the queued call
-    const queuedArgs = queuedArgsRef.current;
-    if (queuedArgs) {
-      queuedArgsRef.current = null;
-      executeQueued && callbackRef.current(...queuedArgs); // Call callback last, in case it throws
+  throttled.__callback = callback;
+  throttled.__waitMs = waitMs;
+
+  return throttled;
+}
+
+export type ThrottleState = "idle" | "running" | "cooldown" | "queued";
+
+function createThrottler<Args extends unknown[]>(setState?: (state: ThrottleState) => void) {
+  let queuedArgs: Args | undefined;
+  let timeoutRef: NodeJS.Timeout | number = 0;
+  let lastCallTime = -9e99;
+
+  throttled.__callback = undefined! as (...args: Args) => void | Promise<void>;
+  throttled.__waitMs = undefined! as number;
+
+  return throttled;
+
+  function throttled(...args: Args) {
+    const wasAlreadyQueued = !!queuedArgs;
+    // set the queued args to the new args
+    queuedArgs = args;
+
+    // if currently running, or if the next call is already scheduled, do nothing else
+    if (!lastCallTime || wasAlreadyQueued) return;
+
+    // clear the timeout, if waiting to set "idle" state
+    if (timeoutRef) clearTimeout(timeoutRef);
+
+    // check if the next call is due
+    const cooldownLeft = throttled.__waitMs - (performance.now() - lastCallTime);
+
+    if (cooldownLeft <= 0) {
+      // run it now with new args
+      popQueueAndRun();
+    } else {
+      // set "queued" state, and start the timeout for the queued call
+      setState?.("queued");
+      timeoutRef = setTimeout(popQueueAndRun, cooldownLeft);
     }
-  }, []);
+  }
+  function popQueueAndRun() {
+    // Clear the timeout id
+    timeoutRef = 0;
+    // Pop the next queued arguments
+    const nextArgs = queuedArgs!;
+    queuedArgs = undefined;
+    // Set the "running" state
+    setState?.("running");
+    // Set lastCallTime to 0, to indicate that the call is in progress
+    lastCallTime = 0;
 
-  return [state, start, reset];
+    try {
+      // Run the callback
+      const result = throttled.__callback(...nextArgs);
+      // When it's finished, set the last call time
+      result instanceof Promise ? result.then(setLastCallTime, setLastCallTime) : setLastCallTime();
+    } catch (err) {
+      setLastCallTime();
+      throw err;
+    }
+  }
+  function setLastCallTime() {
+    lastCallTime = performance.now();
+    // if there's already a next call queued, set "queued" state, and start the timeout again
+    if (queuedArgs) {
+      setState?.("queued");
+      timeoutRef = setTimeout(popQueueAndRun, throttled.__waitMs);
+    } else if (setState) {
+      // when tracking state, set "cooldown" state, and start the timeout to set "idle" state
+      setState("cooldown");
+      timeoutRef = setTimeout(() => ((timeoutRef = 0), setState("idle")), throttled.__waitMs);
+    }
+  }
 }
