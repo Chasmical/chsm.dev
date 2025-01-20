b937323d5d1426d79e684c836cb0446e5e2c49f9
Chasmical <hillankeyxl2@yandex.ru>
2025-01-20 23:23:48 +0700

    ⚡️ Improve performance of some basic hooks

--- lib/hooks/useEventThroughRef.ts
+++ lib/hooks/useEventThroughRef.ts
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import useLatest from "@lib/hooks/useLatest";
 
 type PassiveByDefaultEvent = "wheel" | "touchstart" | "touchmove";
 
@@ -13,32 +13,23 @@ export default function useEventThroughRef<Target extends HTMLElement, Type exte
   type: Type,
   handler: (this: Target, event: HTMLElementEventMap[Type]) => void,
 ): React.Ref<Target> {
-  // Store several state variables in the same ref
-  const stateRef = useRef<InternalState<Target, Type>>(null!);
-  const state = (stateRef.current ??= {});
-  state.handler = handler;
+  const handlerRef = useLatest(handler);
 
   return target => {
     // Run the default ref logic
-    ref && (typeof ref === "function" ? ref(target) : (ref.current = target));
+    let defaultCleanup: void | (() => void);
+    ref && (typeof ref === "function" ? (defaultCleanup = ref(target)) : (ref.current = target));
 
-    // Re-attach the event listener if the target changed
-    if (target !== state.target) {
-      function listener(this: Target, event: HTMLElementEventMap[Type]) {
-        state.handler!.apply(this, [event]);
-      }
-
-      // Remove the old listener, and attach the new one
-      state.target?.removeEventListener(type, state.listener! as never);
-      target?.addEventListener(type, listener as never);
-      state.target = target;
-      state.listener = listener;
+    function listener(this: Target, event: HTMLElementEventMap[Type]) {
+      handlerRef.current.apply(this, [event]);
     }
-  };
-}
+    // Attach an event listener to the target
+    target?.addEventListener(type, listener as never);
 
-interface InternalState<Target extends HTMLElement, Type extends PassiveByDefaultEvent> {
-  handler?: (this: Target, event: HTMLElementEventMap[Type]) => void;
-  listener?: (this: Target, event: HTMLElementEventMap[Type]) => void;
-  target?: Target | null;
+    // On clean-up, remove the event listener, and run the default clean-up as well
+    return () => {
+      target?.removeEventListener(type, listener as never);
+      defaultCleanup?.();
+    };
+  };
 }
